63a0f0cc7a448aa820ac77e7271451a6cf342133
Martialis39 <mart_lepanen@hotmail.com>
2018-08-11 17:29:26 +0300

    11/08

---
+++ buildASquare.js
@@ -0,0 +1,15 @@
+// Build a square /  7th Kyu
+// I will give you an integer. Give me back a shape that is as long and wide as the integer. 
+// The integer will be a whole number between 0 and 50.
+// Example: Integer = 3; I expect a 3x3 square back just like below as a string.
+
+function generateShape(int) {
+    let result = '';
+    for (let i = 0; i < int; i++) {
+        for (let j = 0; j < int; j++) {
+            result += '+';
+        }
+        result += '\n';
+    }
+    return result.slice(0, -1)
+}
